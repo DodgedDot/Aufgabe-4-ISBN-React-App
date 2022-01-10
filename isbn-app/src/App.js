88eb9d26f5ddc7a8b3dc4f5ba2d4009e56d401c7
Tim B <t.beuke15433@hs-ansbach.de>
2022-01-10 20:39:11 +0100

    fullfills task b

--- isbn-app/src/App.js
+++ isbn-app/src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+const {isbn10Checksum, isbn13Checksum} = require('isbn-check/src/isbn-check')
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input id="inputFieldISBN" type="text"></input>
+        <button id="checkButton" onClick={checkISBN}>Check</button>
+        <text id="userFeedback">Feedback</text>
+        
       </header>
     </div>
   );
+  
 }
 
+function checkISBN(){
+  var checkFeedback
+  const inputFieldISBN = document.getElementById('inputFieldISBN')
+  const userFeedback = document.getElementById('userFeedback') 
+  const inputLength = inputFieldISBN.value.split('')
+  if(inputLength.length < 12){
+    checkFeedback = isbn10Checksum(inputFieldISBN.value)
+  } else{
+    checkFeedback = isbn13Checksum(inputFieldISBN.value)
+  }
+  //Uncaught Error: isbn13 does not contain 12 digits to calculate?
+  //Input X as checknumber retruns NaN?
+console.log(checkFeedback)
+  if(checkFeedback === '0'){
+    userFeedback.textContent = "ISBN is valid"
+    userFeedback.style.color = "green"
+  } else{
+    userFeedback.textContent = "ISBN is invalid"
+    userFeedback.style.color = "red"
+  }
+}
+//export {checkISBN};
+//import checkISBN from './App.js';
 export default App;
+
